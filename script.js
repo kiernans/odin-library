a6f9f09d9a6c6538b221e94fe99165303fc0c982
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-06-09 11:21:37 -0700

    Add eventlistener to submit button to add book to library

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ let myLibrary = [];
 const table = document.querySelector('table');
 const button = document.querySelector('button');
 
-function Book(title, author) {
+function Book(title, author, status) {
     this.title = title;
     this.author = author;
+    this.status = status;
 };
 
 function addBookToLibrary(book, array) {
@@ -17,6 +18,16 @@ function printLibrary(array) {
     }
 };
 
-function setBook() {
-    
+function makeBook(title, author, status) {
+    const myBook = new Book(title, author, status);
+    return myBook;
 }
+
+button.addEventListener('click', () => {
+    const title = document.querySelector('#title');
+    const author = document.querySelector('#author');
+    const status = document.querySelector('#status');
+
+    const myBook = makeBook(title.value, author.value, status.value);
+    addBookToLibrary(myBook, myLibrary);
+});
