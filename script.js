bd567f2bd01ca1d5d88a838a404bd80ed3301844
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-06-09 10:56:44 -0700

    Set up form

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 let myLibrary = [];
+const table = document.querySelector('table');
+const button = document.querySelector('button');
 
-const Book = () => {
-    //constructor
+function Book(title, author) {
+    this.title = title;
+    this.author = author;
 };
 
-const addBookToLibrary = () => {
-
+function addBookToLibrary(book, array) {
+    array.push(book);
 };
+
+function printLibrary(array) {
+    for(let i = 0; i < array.length; i++) {
+        console.table(array[i]);
+    }
+};
+
+function setBook() {
+    
+}
